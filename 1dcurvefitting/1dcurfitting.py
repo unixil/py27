2ea25fab5e18934d90c360b3e748c989b9e055c9
unixil <nixil.reg@gmail.com>
2015-06-08 22:14:19 -0300

    a infant curvefitting file that can do nothing and this situition will last for a long time

--- 1dcurvefitting/1dcurfitting.py
+++ 1dcurvefitting/1dcurfitting.py
@@ -4,4 +4,8 @@ Created on Mon Jun  8 21:34:53 2015
 
 @author: pi
 """
-
+import matplotlib.pyplot as plt
+from astropy.table import Table
+t=Table.read('/Volumes/PI/formula/py27/sn2011jh216_20120103.txt',format='ascii')
+print t.colnames
+plt.plot(t['col1'],t['col2'])
